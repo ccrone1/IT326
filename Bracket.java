8fd6fa52495e3e115b1f8e61c5b1be2ee9bbe781
Hmessinger <158091411+Hmessinger@users.noreply.github.com>
2024-03-10 13:30:11 -0500

    Add files via upload

---
+++ Bracket.java
@@ -0,0 +1,19 @@
+public class Bracket {
+
+    private int numOfPlayers;
+
+    // Constructor
+    public Bracket(Event event) {
+
+    }
+
+    // Method to edit the bracket
+    public void editBracket() {
+
+    }
+
+    // Method to update the bracket
+    public void updateBracket() {
+
+    }
+}
---
+++ Tag.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Tag {
+
+    private ArrayList<String> tags;
+    // private String recommendedSkillLevel;
+    // private boolean availableEquipment;
+    // private String otherComments;
+
+    // Constructor
+    public Tag(ArrayList<String> tags) {
+        this.tags = tags;
+    }
+
+    // Method to add tags to an Event
+    public void addTags(Event event) {
+
+    }
+}
